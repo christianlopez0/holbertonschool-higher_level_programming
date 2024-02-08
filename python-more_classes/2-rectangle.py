9a7195b03d0d7db3f35caf32a74f374ee71974df
Brook <christianlopezrivera0@gmail.com>
2024-02-07 21:57:36 -0400

    task 0-2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
-    """Represent a rectangle"""
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
