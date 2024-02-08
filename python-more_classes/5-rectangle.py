728b928bc22e84113111f7a2b0eb65355c0171c6
Brook <christianlopezrivera0@gmail.com>
2024-02-07 22:04:00 -0400

    task 0-2

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -10,14 +10,14 @@ class Rectangle:
 
         Args:
             width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+            height (int): The height of the new rectangle
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set the width of the Rectangle."""
+        """Get/set the width of the Rectangle"""
         return self.__width
 
     @width.setter
