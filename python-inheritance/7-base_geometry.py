cc5d15d655550dc321c0ffeabf69f0f5761369a5
Brook <christianlopezrivera0@gmail.com>
2024-02-08 21:41:45 -0400

    task fixing

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-# 7-base_geometry.py
-
 """Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """Reprsent base geometry"""
+    """Reprsent base geometry."""
 
     def area(self):
         """Not yet implemented."""
