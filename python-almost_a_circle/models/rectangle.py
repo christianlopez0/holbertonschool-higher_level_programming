e75a77e469c81c93b7ad1ab647bc5468a7e655e6
Brook <christianlopezrivera0@gmail.com>
2024-02-15 18:57:26 -0400

    fixing and pushing to test checkers

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,80 +1,97 @@
-#!/usr/bin/python3
-'''
-    Class Rectangle
-'''
+#!/bin/usr/python3
+"""
+Rectangle module
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''
-        Defining the Rectangle class
-        Inherits from:
-            Base
-    '''
-
+    """
+    Rectangle class
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+        """
+        super().__init__(id)
+
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
-        '''
-            Returning private attribute
-        '''
+        """
+        width getter
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("width", value)
+        """
+        Width setter
+        """
+        # added a check for when value is a bool, if the check is removed
+        # the unittest for it will fail
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
-        '''
-            Returning private attribute
-        '''
+        """
+        height getter
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("height", value)
+        """
+        height setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
         self.__height = value
 
     @property
     def x(self):
-        '''
-            Returning private attribute
-        '''
+        """
+        x getter
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("x", value)
+        """
+        x setter
+        """
+        # added a check for when value is a bool, if the check is removed
+        # the unittest for it will fail
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        '''
-            Returning private attribute
-        '''
+        """
+        y getter
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("y", value)
+        """
+        y setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
