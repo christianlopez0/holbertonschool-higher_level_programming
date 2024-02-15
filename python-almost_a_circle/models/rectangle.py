9704df794d66e6eac0b968f7c0e13bec9de7e407
Brook <christianlopezrivera0@gmail.com>
2024-02-15 18:31:28 -0400

    fixing and pushing to test checkers

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-
+"""
+Rectangle module
+"""
 from base import Base
 
 class Rectangle(Base):
-    
+    """
+    Rectangle class
+    """
  def __init__(self, width, height, x=0, y=0, id=None):
     super().__init__(id)
 
