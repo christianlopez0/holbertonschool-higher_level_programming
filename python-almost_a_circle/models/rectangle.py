5c6b9119eb050194049c08530b07ee4f589b7a69
Brook <christianlopezrivera0@gmail.com>
2024-02-15 18:42:25 -0400

    fixing and pushing to test checkers

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,14 +9,14 @@ class Rectangle(Base):
     Rectangle class
     """
  def __init__(self, width, height, x=0, y=0, id=None):
-    super().__init__(id)
-
-    self.width = width
-    self.height = height
-    self.x = x
-    self.y = y
-
-
+        super().__init__(id)  # Call the constructor of the Base class
+        
+        # Assigning attributes
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+    
     @property
     def width(self):
         return self.__width
