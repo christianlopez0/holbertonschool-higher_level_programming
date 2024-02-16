eb53dd8c8162a917bcd2def75534cd6dcd8b2c66
Brook <christianlopezrivera0@gmail.com>
2024-02-15 20:02:54 -0400

    fixing and pushing to test checkers

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,66 +1,59 @@
 #!/usr/bin/python3
-"""
-    Rectangle class
-    """
+#!/usr/bin/python3
+""" rectangle class"""
 from models.base import Base
 
+
 class Rectangle(Base):
+    """makes a new rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
+        """initializes"""
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-    
+        super().__init__(id)
+  
     @property
     def width(self):
+        """get width"""
         return self.__width
-    
-    @width.setter
-    def width(self, width):
-        if not isinstance(width, (int, float)):
-            raise ValueError("Width must be a number.")
-        if width <= 0:
-            raise ValueError("Width must be positive.")
-        self.__width = width
-    
+
     @property
     def height(self):
+        """get height"""
         return self.__height
-    
-    @height.setter
-    def height(self, height):
-        if not isinstance(height, (int, float)):
-            raise ValueError("Height must be a number.")
-        if height <= 0:
-            raise ValueError("Height must be positive.")
-        self.__height = height
-    
+
     @property
     def x(self):
+        """get x"""
         return self.__x
-    
-    @x.setter
-    def x(self, x):
-        if not isinstance(x, (int, float)):
-            raise ValueError("X coordinate must be a number.")
-        self.__x = x
-    
+
     @property
     def y(self):
+        """get y"""
         return self.__y
-    
+
+    @width.setter
+    def width(self, value):
+        """sets width"""
+        self.validator("width", value)
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """sets height"""
+        self.validator("height", value)
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        self.validator("x", value)
+        """sets x"""
+        self.__x = value
+
     @y.setter
-    def y(self, y):
-        if not isinstance(y, (int, float)):
-            raise ValueError("Y coordinate must be a number.")
-        self.__y = y
-
-
-if __name__ == "__main__":
-    rect = Rectangle(10, 20, 5, 5, id=1)
-    print(rect.width)   
-    print(rect.height)  
-    print(rect.x)       
-    print(rect.y)  
-    print(rect.id) 
+    def y(self, value):
+        """sets y"""
+        self.validator("y", value)
+        self.__y = value
