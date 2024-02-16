b6cb81a9a2114ecb0053b18548ac3929e022d753
Brook <christianlopezrivera0@gmail.com>
2024-02-15 20:08:39 -0400

    fixing and pushing to test checkers

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,58 +1,59 @@
 #!/usr/bin/python3
-""" rectangle class"""
+"""Rectangle class"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """makes a new rectangle"""
+    """Creates a new rectangle"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """initializes"""
+        """Initializes the rectangle"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
   
     @property
     def width(self):
-        """get width"""
+        """Getter for width"""
         return self.__width
 
     @property
     def height(self):
-        """get height"""
+        """Getter for height"""
         return self.__height
 
     @property
     def x(self):
-        """get x"""
+        """Getter for x"""
         return self.__x
 
     @property
     def y(self):
-        """get y"""
+        """Getter for y"""
         return self.__y
 
     @width.setter
     def width(self, value):
-        """sets width"""
+        """Setter for width"""
         self.validator("width", value)
         self.__width = value
 
     @height.setter
     def height(self, value):
-        """sets height"""
+        """Setter for height"""
         self.validator("height", value)
         self.__height = value
 
     @x.setter
     def x(self, value):
+        """Setter for x"""
         self.validator("x", value)
-        """sets x"""
         self.__x = value
 
     @y.setter
     def y(self, value):
-        """sets y"""
+        """Setter for y"""
         self.validator("y", value)
         self.__y = value
