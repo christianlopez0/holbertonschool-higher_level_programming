6fcfc6b06d0d1c06de21d69a6fcb3f2b363c526a
Brook <christianlopezrivera0@gmail.com>
2024-02-16 01:49:20 -0400

    adding test rectangle and testing test answer task 9

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -85,15 +85,12 @@ class Rectangle(Base):
         """Returns a string representation of the rectangle"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
     
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Assigns arguments to attributes"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+        if args:
+            attrs = ['id', 'width', 'height', 'x', 'y']
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
