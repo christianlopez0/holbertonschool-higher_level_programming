42f14c9f37a714392dd0d8ff283c45006ba2cb74
Brook <christianlopezrivera0@gmail.com>
2024-02-16 13:38:41 -0400

    adding test square and testing test answer task 12

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -1,26 +1,65 @@
 #!/usr/bin/python3
+"""Define Rectangle Class
+"""
+
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
-    """Creates a new square"""
+    """Module Representation of Square
+"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializes the square"""
+        """Initialization a Square
+        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """Getter for size"""
+        """module Square size getter
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """Setter for size"""
+        """module Square size setter
+        """
         self.width = value
         self.height = value
 
     def __str__(self):
-        """Returns a string representation of the square"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        """module string represation of square
+        """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
 
+    def update(self, *args, **kwargs):
+        """module update square
+        """
+        if len(args):
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """retrun dictonary
+        """
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
 
