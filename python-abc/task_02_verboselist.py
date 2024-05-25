a1c63db1420a3384bad75385c0bc6cdd4dd694bc
Christian lopez <christianlopezrivera0@gmail.com>
2024-05-24 20:08:21 -0400

    pythonabc fixing task3

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -24,12 +24,3 @@ class VerboseList(list):
             print(f"Popped {item} from the list.")
         return super().pop(index)
 
-
-# Test
-verbose_list = VerboseList()
-verbose_list.append(1)
-verbose_list.extend([2, 3, 4])
-verbose_list.remove(3)
-verbose_list.remove(5) 
-verbose_list.pop()
-verbose_list.pop(0)
